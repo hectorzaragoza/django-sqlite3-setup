da5c4337c1fc0badacda39c5063da0206531ee4a
hectorzaragoza <zaragoza09@gmail.com>
2021-12-29 08:03:40 -0800

    Added db functionality, addtl models, 1:1, 1:M, M:M relationships

--- book_outlet/admin.py
+++ book_outlet/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Book, Author, Address, Country
+
+class BookAdmin(admin.ModelAdmin):
+    # readonly_fields = ("slug",)
+    prepopulated_fields = {"slug": ("title",)}
+    list_filter = ("author", "rating",)
+    list_display = ("title", "author",)
+
+admin.site.register(Book, BookAdmin)
+admin.site.register(Author)
+admin.site.register(Address)
+admin.site.register(Country)
